2c0f4c5f40367cc3c777a1f33d75c10c02cd3950
Aaron Hill <aa1ronham@gmail.com>
2013-09-24 16:22:33 -0400

    Added Tiled maps

---
+++ client/data/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="000080_Navy_Blue_Square" tilewidth="32" tileheight="32">
+ <image source="../../../../Downloads/000080_Navy_Blue_Square.svg" width="600" height="600"/>
+</tileset>
