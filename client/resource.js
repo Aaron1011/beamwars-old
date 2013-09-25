a372ded6347124f8c9b4161c4537da1357623c7f
Aaron Hill <aa1ronham@gmail.com>
2013-09-25 16:40:58 -0400

    Added new image resources

--- client/resource.js
+++ client/resource.js
@@ -2,7 +2,9 @@ define([], function() {
     var Resource =  [
         {name: "map", type: "tmx", src: "data/map.tmx"},
 
-        {name: "map_level_tiles", type: "image", src: "data/map.png"}
+        {name: "map_level_tiles", type: "image", src: "data/map.png"},
+        {name: "mainPlayer_image", type: "image", src: "data/player_image.png"},
+        {name: "metatiles", type: "image", src: "data/metatiles.png"}
 
     ];
     return Resource;
