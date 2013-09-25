35f1e447d042cb9f8ff74911583261bcd2b1a674
Aaron Hill <aa1ronham@gmail.com>
2013-09-24 20:54:51 -0400

    Added resource.js

---
+++ client/resource.js
@@ -0,0 +1,9 @@
+define([], function() {
+    var Resource =  [
+        {name: "map", type: "tmx", src: "data/map.tmx"},
+
+        {name: "map_level_tiles", type: "image", src: "data/map.png"}
+
+    ];
+    return Resource;
+});
